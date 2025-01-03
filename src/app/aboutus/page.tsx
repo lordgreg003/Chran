dfbf499d8aacc7db798c116ccdb1bfab22b258aa
chidera nwokoye <chideranwokoye555@gmail.com>
2025-01-03 07:32:06 +0100

    resolved conflicts

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,19 @@
+ import React from 'react';
+import Navbar from '../ui/components/layoutComponents/navbar';
+import CHRNAboutSection from '../ui/components/aboutComponents/CHRNAboutSection';
+import Footer from '../ui/components/layoutComponents/Footer';
+import Mission from '../ui/components/aboutComponents/Mission';
+ 
+ const page = () => {
+   return (
+     <div>
+       <Navbar />
+      <Mission />
+      <CHRNAboutSection />
+      <Footer />
+     </div>
+   )
+ }
+ 
+ export default page
+ 
