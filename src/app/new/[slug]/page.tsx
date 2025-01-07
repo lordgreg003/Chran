d1dc5f01d20440717c398b8327b1cca0019247d1
chidera nwokoye <chideranwokoye555@gmail.com>
2025-01-07 23:05:04 +0100

    resolved conflicts

--- src/app/new/[slug]/page.tsx
+++ src/app/new/[slug]/page.tsx
@@ -115,3 +115,14 @@
 //     return <p className="text-center text-red-500">An error occurred.</p>;
 //   }
 // }
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
