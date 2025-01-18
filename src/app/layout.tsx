1452af99bf62b36bf5b765657fd682d9c32ab643
chidera <chideranwokoye555@gmail.com>
2025-01-18 00:42:37 -0500

    googletagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next";
 import "./globals.css";
 import "animate.css";
 import ReduxProvider from "./ui/utils/ReduxProvider";
 import Footer from "./ui/components/layoutComponents/Footer";
 import Navbar from "./ui/components/layoutComponents/navbar";
-import Head from "next/head";  // Import Head from Next.js
+import Head from "next/head";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.chran.org"),
   title: { default: "Chran", template: "%s | Explore Chran" },
-  description: "The Center For HUman Rights & Accountability Network",
+  description: "The Center For Human Rights & Accountability Network",
   openGraph: {
     title: { default: "Chran", template: "%s | Explore Chran" },
     description: "Implemented Chidera",
@@ -19,6 +20,12 @@ export const metadata: Metadata = {
   },
 };
 
+// Declare global variables
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+  }}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
@@ -26,12 +33,28 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Head>
-        <meta name="google-adsense-account" content="ca-pub-8660328606992175" />
+          <meta name="google-adsense-account" content="ca-pub-8660328606992175" />
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8660328606992175"
             crossOrigin="anonymous"
           ></script>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-Q1PCGJX7KR"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(...args) {
+                  window.dataLayer.push(args);
+                }
+                gtag('js', new Date());
+                gtag('config', 'G-Q1PCGJX7KR');
+              `,
+            }}
+          />
         </Head>
       </head>
       <body>
